992d6feee5c502db890130c399ae93c49cf3b125
ashu1221 <67498019+ashu1221@users.noreply.github.com>
2020-07-31 21:34:57 +0530

    Add files via upload

---
+++ tut_1.py
@@ -0,0 +1,21 @@
+print("Hello World")
+print("Ashutosh")
+print("Ashutosh",end="Sharma")
+# use # for single line comment
+# how use  multiline comment
+"""
+this 
+is 
+multiline comments
+"""""
+g= input("enter your name")
+print(g)
+# variable of python
+var1=5 #int variable
+var2=5.6 #float variable
+var3="Ashutosh" #string type
+var4=False #boolean type
+var1=3
+print(var1,var2,var3,var4)
+#find data type
+print(type (var1))
